e8e3216dda4a9f76c52df9f88a156ccea569c812
Kenisson P Favero <kenissonpfavero@Kenissons-MacBook-Air.local>
2022-10-10 20:51:21 -0300

    w3 css template aplicado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,17 +3,25 @@
 
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="https://cdn.tailwindcss.com"></script>
-  <title>@yield('title') - CongReports</title>
-  <!-- Fonts -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+  <style>
+    body,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5 {
+      font-family: "Raleway", sans-serif
+    }
+  </style>
 </head>
 
-<body class="antialiased">
-  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+<body class="w3-light-grey">
+  <div class="container mx-auto px-20 py-8 font-semibold w3-padding-16">
     @yield('content')
   </div>
-  <br>
   <br>
   <p><a href="{{route('index')}}">Volver al inicio</a></p>
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,34 +1,29 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <title>CongReports</title>
+@extends('layouts.app')
+@section('title', 'CongReports')
+@section('content')
 
 </head>
 
-<body>
-  <h1>Bienvenido a CongReports</h1>
-  <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
-    <div class="grid grid-cols-1 md:grid-cols-2">
-      <div class="p-6">
-        <div class="flex items-center">
-          <div class="ml-4 text-lg leading-7 font-semibold">
-            <p><a href="{{ 'publicadores' }}" class="underline text-gray-900 dark:text-white">Ver Publicadores</a></p>
-            <p><a href="{{route('publicadores.create')}}" class="underline text-gray-900 dark:text-white">Crear publicador</a></p>
-          </div>
-        </div>
-
-        <div class="ml-12">
-          <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-            Facilitando la vida del secretario!
-          </div>
-        </div>
-      </div>
+<body class="bg-gray-50">
+  <!-- Header -->
+  <header class="w3-container w3-center w3-padding-32">
+    <h1><b>Bienvenido a CongReports</b></h1>
+    <p>Facilitando la vida del secretario!
+    </p>
+  </header>
+  <h1></h1>
+  <div class="w3-card w3-margin">
+    <div class="w3-container w3-padding">
+      <h4><a href="{{ 'publicadores' }}" class="underline text-gray-900 dark:text-white">Publicadores</a></h4>
+    </div>
+    <div class="w3-container w3-padding">
+      <h4><a href="{{route('publicadores.create')}}" class="underline text-gray-900 dark:text-white">Crear publicador</a></h4>
     </div>
   </div>
-</body>
+  <hr>
+  <p></p>
+  <p></p>
+  </div>
+  </div>
 
-</html>
+  @endsection
